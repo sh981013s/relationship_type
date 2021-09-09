bc714a6a4d1899012e08a44e72131e4abf91342c
SeungHwan-Lee <sh981013s@gmail.com>
2021-09-09 15:09:23 +0900

    .

--- assets/js/share.js
+++ assets/js/share.js
@@ -1,36 +1,38 @@
 function kakaoShare() {
     console.log(11);
-    Kakao.Link.sendDefault({
-        objectType: 'feed',
-        content: {
-        title: '디저트 사진',
-        description: '아메리카노, 빵, 케익',
-        imageUrl:
-            'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-        link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            androidExecutionParams: 'test',
-        },
-        },
-        social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
-        },
-        buttons: [
-        {
-            title: '웹으로 이동',
-            link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            },
-        },
-        {
-            title: '앱으로 이동',
-            link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            },
-        },
-        ]
+    Kakao.Link.sendScrap({
+        // objectType: 'feed',
+        // content: {
+        // title: '디저트 사진',
+        // description: '아메리카노, 빵, 케익',
+        // imageUrl:
+        //     'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        // link: {
+        //     mobileWebUrl: 'https://developers.kakao.com',
+        //     androidExecutionParams: 'test',
+        // },
+        // },
+        // social: {
+        // likeCount: 10,
+        // commentCount: 20,
+        // sharedCount: 30,
+        // },
+        // buttons: [
+        // {
+        //     title: '웹으로 이동',
+        //     link: {
+        //     mobileWebUrl: 'https://developers.kakao.com',
+        //     },
+        // },
+        // {
+        //     title: '앱으로 이동',
+        //     link: {
+        //     mobileWebUrl: 'https://developers.kakao.com',
+        //     },
+        // },
+        // ]
+        requestUrl: 'https://12animalsrelationship.netlify.app/',
+
     });
 }
 
